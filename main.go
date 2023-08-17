f05662b81a8084f44dfcaca3275ccd738e443623
zhangweijie <15538088057@163.com>
2023-08-17 17:30:07 +0800

    1、Improve business processe

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"icp/beian"
+	"icp/chinaz"
+	"icp/crawler"
 	"icp/esicp"
+	"icp/icpapi"
 )
 
 // GetIcpInfo 获取 ICP 数据
-func GetIcpInfo(domain string) *esicp.ESIcpInfo {
-	esIcpInfo, _ := beian.GetBeianIcpInfo(domain)
+func GetIcpInfo(domain string, rodScraper *crawler.RodScraper) *esicp.ESIcpInfo {
+	esIcpInfo, err := beian.GetBeianIcpInfo(domain)
+	if esIcpInfo == nil && errors.Is(err, esicp.ErrDomainNotRegistered) {
+		if rodScraper == nil {
+			rodScraper = crawler.NewRodScraper()
+		}
+		icpSources := []string{esicp.SourceIcpApi, esicp.SourceChinaz}
+		for _, icpSource := range icpSources {
+			if icpSource == esicp.SourceIcpApi {
+				esIcpInfo = icpapi.GetIcpapiIcpInfo(domain, rodScraper)
+				if esIcpInfo != nil {
+					break
+				}
+			}
+
+			if icpSource == esicp.SourceChinaz {
+				esIcpInfo = chinaz.GetChinazIcpInfo(domain, rodScraper)
+				if esIcpInfo != nil {
+					break
+				}
+			}
+		}
+
+		if rodScraper != nil {
+			rodScraper.Close()
+		}
+	}
 
 	return esIcpInfo
 }
 
 func main() {
-	icpInfo := GetIcpInfo("socmap.net")
+	icpInfo := GetIcpInfo("socmap.net", nil)
 	fmt.Println("------------>", icpInfo)
 }
